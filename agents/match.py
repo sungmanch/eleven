99d0de28c34c7c32b1d7bd52be22bbdc37a599f5
sungmanch <sungman.cho@latched.ai>
2025-02-22 15:14:26 +0900

    Add dummy conversation

--- agents/match.py
+++ agents/match.py
@@ -1,12 +1,18 @@
 import json
 
 from type.user_info import UserInfo
+from langchain_openai import ChatOpenAI
 
 class MatchAgent:
     def __init__(self):
         self.users_db = self._load_users_db()
+        self.llm = ChatOpenAI(
+            model="gpt-4o",
+            temperature=0.75,
+        )
 
     def _load_users_db(self) -> list[UserInfo]:
+        #TODO: Need to change to actual db
         with open("dummy_user_db.json", "r") as f:
             users_data = json.load(f)
             return [UserInfo(**user) for user in users_data]
@@ -18,3 +24,7 @@ class MatchAgent:
         call_history: list[str],
     ):
         pass
+
+if __name__ == "__main__":
+    match_agent = MatchAgent()
+    user_info = UserInfo(firstname="Daniel", age=38, gender="Male", location="New York")
--- type/user_info.py
+++ type/user_info.py
@@ -1,10 +1,10 @@
 class UserInfo:
     def __init__(self, **kwargs):
-        self.name = kwargs.get("name")
+        self.firstname = kwargs.get("firstname")
         self.age = kwargs.get("age")
         self.gender = kwargs.get("gender")
         self.location = kwargs.get("location")
         self.id = kwargs.get("id")
 
     def __str__(self):
-        return f"UserInfo(name={self.name}, age={self.age}, gender={self.gender}, location={self.location})"
+        return f"UserInfo(firstname={self.firstname}, age={self.age}, gender={self.gender}, location={self.location})"
